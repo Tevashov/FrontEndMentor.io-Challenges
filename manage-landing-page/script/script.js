670acf91bb23daee8dbea76a64ce7d8a5478ca0a
Teff <tevashov@gmail.com>
2022-11-16 22:36:47 +0300

    A11Y Scroller, error message in footer

--- manage-landing-page/script/script.js
+++ manage-landing-page/script/script.js
@@ -1,33 +1,32 @@
-(function() {
-    addEventListener( 'DOMContentLoaded', (event) => {
 
-        if ( document.getElementById( 'js-reviews' )) {
+document.querySelector('#js-submit').addEventListener('click', function(e) {
+    document.querySelector('#js-message').classList.toggle('is-hidden');
+});
 
-            let $reviewsBox = document.getElementById( 'js-reviews' ),
-                len = $reviewsBox.children.length,
-                scrollLTR = true,
-                i = 0,
-                scrollDelta = 0,
-                scrollWidth = $reviewsBox.clientWidth,
-                bodyWidth = document.body.clientWidth,
-                maxScroll = scrollWidth - bodyWidth;
 
-            function setScroll() {
-                if ( scrollLTR ) {
-                    i++;
-                    scrollDelta = ( scrollDelta > maxScroll ) ? maxScroll : scrollDelta + maxScroll / len;
-                } else {
-                    i--;
-                    scrollDelta = ( scrollDelta < 0 ) ? 0 : scrollDelta - maxScroll / len
-                }
-                if ( i == len || i < 1 ) scrollLTR = !scrollLTR;
-                $reviewsBox.style.left = '-' + scrollDelta + 'px';
-            }
+const sliderEl = document.querySelector('#js-reviews');
 
-            if ( scrollWidth > bodyWidth ) doScroll = setInterval( setScroll, 5000 );
-            // TODO on window resize check width and do start or stop
-
-        } // :: END getElementById( 'js-reviews' )
-
-    }); // :: END DOMContentLoaded
-})();
+const slider = new A11YSlider(sliderEl, {
+    autoplay: true,
+    arrows: false,
+    adaptiveHeight: true,
+    infinite: true,
+    dots: true,
+    responsive: {
+        500: { // 500 to 740
+            slidesToShow: 1,
+        },
+        740: { // 740 to 960
+            slidesToShow: 2,
+        },
+        960: { // 960px to 1279px
+            slidesToShow: 2,
+        },
+        1280: { // 1280px and up
+            slidesToShow: 3,
+        },
+        1440: { // 1280px and up
+            slidesToShow: 4,
+        }
+    }
+});
